03446bd2ea15c8d54c28e1fc3b79a5aa0b39dae2
ZechariahZheng <1173478308@qq.com>
2018-08-02 16:57:54 +0800

    题目：从上往下打印二叉树 编译是否通过：是
    注：
    二叉树的层次遍历使用列表做中间过度

---
+++ 22.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        # write code here 二叉树的层次遍历
+        all = []
+        if not root:
+            return all
+        q = [root]
+        while len(q):
+            t = q.pop(0)
+            all.append(t.val)
+            if t.left:
+                q.append(t.left)
+            if t.right:
+                q.append(t.right)
+        return all
