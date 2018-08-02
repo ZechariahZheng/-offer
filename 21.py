3b6221948ff614a852b4c33e38f626f8739cda39
ZechariahZheng <1173478308@qq.com>
2018-08-02 16:25:07 +0800

    题目：栈的压入、弹出序列 编译是否通过：是
    注：
    python中的列表遍历
    for i in list:

---
+++ 21.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def IsPopOrder(self, pushV, popV):
+        # write code here
+        if not pushV or len(pushV) != len(popV):
+            return False
+        stack = []
+        for i in pushV:
+            stack.append(i)
+            while len(stack) and stack[-1] == popV[0]:
+                stack.pop()
+                popV.pop(0)
+        if len(stack):
+            return False
+        return True
+S = Solution()
+A = [1,2,3,4,5]; B = [3,5,4,2,1]; 
+if S.IsPopOrder(A, B):
+    print("hello")
